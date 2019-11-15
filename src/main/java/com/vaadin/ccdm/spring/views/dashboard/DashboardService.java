d849306ba63c324080f25fa1b057af1913cd90ed
Manolo Carrasco <manolo@apache.org>
2019-11-15 17:53:18 +0100

    Setting restrictions to services

--- src/main/java/com/vaadin/ccdm/spring/views/dashboard/DashboardService.java
+++ src/main/java/com/vaadin/ccdm/spring/views/dashboard/DashboardService.java
@@ -1,5 +1,8 @@
 package com.vaadin.ccdm.spring.views.dashboard;
 
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -11,9 +14,9 @@ import com.vaadin.flow.server.connect.auth.AnonymousAllowed;
  * Services for client-side Dashboard View.
  */
 @VaadinService
-@AnonymousAllowed
 public class DashboardService {
 
+  @AnonymousAllowed
   public List<HealthGridItem> healthGridItems() {
     return Arrays.asList(
       new HealthGridItem(LocalDate.of(2019, 1, 14), "M\u00FCnster", "Germany", "Good", "badge"),
@@ -28,6 +31,7 @@ public class DashboardService {
     );
   }
 
+  @PermitAll
   public List<ChartSeries> responseTimesSeries() {
     return Arrays.asList(
       new ChartSeries("Tokyo", 7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6),
@@ -35,6 +39,7 @@ public class DashboardService {
     );
   }
 
+  @RolesAllowed("ADMIN")
   public List<ChartSeries> monthlyVisitorsSeries() {
     return Arrays.asList(
       new ChartSeries("Tokyo", 49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4,194.1, 95.6, 54.4),
