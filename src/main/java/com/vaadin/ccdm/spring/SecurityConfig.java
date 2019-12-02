fcfe29a93cee5d8eae30b1e67e83c22ad502f34e
Manolo Carrasco <manolo@apache.org>
2019-12-02 23:56:34 +0100

    simplify security

--- src/main/java/com/vaadin/ccdm/spring/SecurityConfig.java
+++ src/main/java/com/vaadin/ccdm/spring/SecurityConfig.java
@@ -1,20 +1,11 @@
 package com.vaadin.ccdm.spring;
 
-import javax.servlet.http.HttpServletRequest;
-
-import java.util.stream.Stream;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-import com.vaadin.flow.server.ServletHelper;
-import com.vaadin.flow.shared.ApplicationConstants;
 
 /**
  * The Spring Boot configuration for authentication part of the Vaadin Connect
@@ -24,61 +15,26 @@ import com.vaadin.flow.shared.ApplicationConstants;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)
-            throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}user")
-                .roles("USER").and().withUser("admin")
-                .password("{noop}admin").roles("ADMIN", "USER");
-    }
-
-    @Bean
-    @Override
-    public UserDetailsService userDetailsServiceBean() throws Exception {
-        return super.userDetailsServiceBean();
-    }
-
-    /**
-     * Require login to access internal pages and configure login form.
-     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Flow and connect already handles csrf.
-        http.csrf().disable();
-        // Allow all flow internal requests.
-        http.authorizeRequests()
-                .requestMatchers(SecurityConfig::isFrameworkInternalRequest)
-                .permitAll();
-        // using default spring login form
-        http.formLogin();
+      // Vaadin already handles csrf.
+      http.csrf().disable();
+      // Use default spring login form
+      http.formLogin();
     }
 
-    /**
-     * Allows access to static resources, bypassing Spring security.
-     */
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
-                // Vaadin Flow static resources
-                "/VAADIN/**");
+    public void configure(WebSecurity web) {
+      // Access to static resources, bypassing Spring security.
+      web.ignoring().antMatchers("/VAADIN/**");
     }
 
-    /**
-     * Tests if the request is an internal framework request. The test consists
-     * of checking if the request parameter is present and if its value is
-     * consistent with any of the request types know.
-     *
-     * @param request
-     *            {@link HttpServletRequest}
-     * @return true if is an internal framework request. False otherwise.
-     */
-    private static boolean isFrameworkInternalRequest(
-            HttpServletRequest request) {
-        final String parameterValue = request
-                .getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
-        return parameterValue != null
-                && Stream.of(ServletHelper.RequestType.values()).anyMatch(
-                        r -> r.getIdentifier().equals(parameterValue));
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      // Configure users and roles in memory
+      auth.inMemoryAuthentication()
+          .withUser("user").password("{noop}user").roles("USER")
+          .and()
+          .withUser("admin").password("{noop}admin").roles("ADMIN", "USER");
     }
 }
